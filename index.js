ae44aef63839feb7a9a37a762a5ef643eef55f3c
Adam Abdel-Halim <adaab301@student.liu.se>
2021-02-09 15:11:50 +0000

    Pug quickstart

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+var pug = require('pug');
+
 const app = express();
 const port = 3000;
+
+var fn = pug.compileFile('views/index.pug');
+
+app.set('view engine', 'pug');
+app.set('views', './views')
+
  
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.render('index', {title: 'Hello', message: 'Yooo'});
 })
  
 app.listen(port, () => {
