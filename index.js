f324f05da8dca3924f366d6e3ddee19240e61e52
Adam Abdel-Halim <adaab301@student.liu.se>
2021-02-09 16:32:00 +0000

    Removed unused code

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.set('view engine', 'pug');
 app.set('views', './views')
 app.use('/scripts', express.static('scripts'))
 
-//pingSMA();
-
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hello', message: '0ms' });
 })
@@ -20,32 +18,3 @@ app.get('/', function (req, res) {
 app.listen(port, () => {
   console.log(`Litening on ${port}.`);
 });
-
-
-async function getPing() {
-  let ping;
-  let start = Date.now();
-  await fetch("https://halim.se/", { mode: 'no-cors' })
-    .then(() => ping = (Date.now() - start));
-  return ping;
-}
-let SMAarray = [];
-async function pingSMA() {  
-  setInterval(async () => {
-    let ping = await getPing();
-    if(SMAarray.length == 10) {
-      SMAarray.pop();
-    }
-    SMAarray.unshift(ping);
-    let avg = SMAarray.reduce((a,b) => a+b) / SMAarray.length;
-    // pug stuff here
-    changePing(avg);
-  }, 100);
-}
-
-async function changePing(ping) {
-  console.log(ping)
-  app.get('/', function (req, res) {
-    res.render('index', { title: 'Hello', message: ping });
-  })
-}
