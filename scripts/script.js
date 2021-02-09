361fb91bb13a55e52654ba53770e4cab0711b4ef
Adam Abdel-Halim <adaab301@student.liu.se>
2021-02-09 19:37:57 +0000

    Now can handle multiple domains

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,34 @@
-async function getPing() {
+async function getPing(url) {
     let ping;
     let start = Date.now();
-    await fetch("https://halim.se/", { mode: 'no-cors' })
+    await fetch(url, { mode: 'no-cors' })
         .then(() => ping = (Date.now() - start));
     return ping;
 }
-let SMAarray = [];
+let SMAarray = [[], []];
+let domainArr = [['https://halim.se/', 'halim'], ['https://wsb.halim.se/', 'wsb']];
 async function pingSMA() {
-    setInterval(async () => {
-        let ping = await getPing();
-        if (SMAarray.length == 10) {
-            SMAarray.pop();
-        }
-        SMAarray.unshift(ping);
-       
-    }, 100);
+    for (let i = 0; i < domainArr.length; i++) {
+        setInterval(async () => {
+            let ping = await getPing(domainArr[i][0]);
+            if (SMAarray[i].length == 10) {
+                SMAarray[i].pop();
+            }
+            SMAarray[i].unshift(ping);
 
-    setInterval(() => {
-        let avg = Math.round(SMAarray.reduce((a, b) => a + b) / SMAarray.length);
-        // pug stuff here
-        changePing(avg);
-    }, 1000);
+        }, 100);
+    }
+    for (let i = 0; i < domainArr.length; i++) {
+        setInterval(() => {
+            let avg = Math.round(SMAarray[i].reduce((a, b) => a + b) / SMAarray[i].length);
+            changePing(domainArr[i][1], domainArr[i][0], avg);
+        }, 500);
+    }
 }
 
-function changePing(ping) {
-    document.getElementById('yo').innerHTML = ping + 'ms';
+function changePing(id, url, ping) {
+    let niceUrl = url.replace(/^https?:\/\//,'');
+    document.getElementById(id).innerHTML = `<a href="${url}">${niceUrl}</a>: ` + ping + ' ms.';
 }
 
 pingSMA();
