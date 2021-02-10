a599ca31874b4b0b10b5e57a18d84f5458bcf61a
Adam Abdel-Halim <adaab301@student.liu.se>
2021-02-10 11:21:12 +0000

    Added script that checks health of LAN

---
+++ core/lanPinger.js
@@ -0,0 +1,36 @@
+const util = require('util');
+var exec = util.promisify(require('child_process').exec);
+
+const lanSites = require('../data/lanSites.json');
+
+/**
+ * Ping a server on the local network
+ * @param {IP address of server} ip 
+ */
+async function getPing(ip) {
+    const { stdout, stderr } = await exec(`ping -c 1 ${ip}`);
+}
+
+/**
+ * Goes through each service on ../data/lanSites.json,
+ * pings them and checks if they're running.
+ * This run once per second.
+ */
+async function aliveStatuses() {
+    setInterval(async () => {
+        let lanStatus = lanSites;
+        for (const [hostname, values] of Object.entries(lanStatus)) {
+            await getPing(values.ip).then(() => {
+                lanStatus[hostname].status = true;
+            }).catch(() => {
+                lanStatus[hostname].status = false;
+            })
+        }
+    }, 1000);
+
+}
+
+module.exports = {
+    getPing,
+    aliveStatuses,
+}
